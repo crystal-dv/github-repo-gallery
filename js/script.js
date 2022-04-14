b16bb8bb344e1825649dde172623d8a34d9718e0
crystal_c <crystalc.dev@gmail.com>
2022-04-14 17:02:04 -0500

    Added click event for selecting an individual repo, added functions to fetch individual repo data and display it in its own section using hide class

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 // Div where profile info will go
 const overview = document.querySelector(".overview");
-
 const username = "crystal-dv";
+//Unordered list where repos will go
+const repoArea = document.querySelector(".repo-list");
+//Selects section where list of repos is displayed
+const repos = document.querySelector(".repos");
+//Section where each individual repo's data will go when selected
+const singleRepo = document.querySelector(".repo-data");
 
-const repoList = document.querySelector(".repo-list");
 
 //Function to fetch profile info from GitHub and call the displayProfile function
 const getProfile = async function () {
@@ -37,7 +41,6 @@ const displayProfile = function(profileData) {
     getRepos();
 };
 
-
 //Function to fetch repo data from GitHub and call to display repo info
 const getRepos = async function () {
     const repoRes = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
@@ -54,6 +57,51 @@ const displayRepos = function (repoData) {
         repo.className = "repo";
         let listItem = document.createElement("li");
         listItem.innerHTML = `<h3>${repo.name}</h3>`;
-        repoList.append(listItem);
+        repoArea.append(listItem);
     };
  };
+
+ //Event listener for clicking on a repo title
+ const repoList = repoArea.addEventListener( "click", function (e) {
+     if (e.target.matches("h3")) {
+         const repoName = e.target.innerText;
+         getRepoInfo(repoName);
+     }
+ });
+
+ //Function to get info about an individual repo
+ const getRepoInfo = async function (repoName) {
+     const repoInfoRes = await fetch (`https://api.github.com/repos/${username}/${repoName}`);
+
+     const repoInfo = await repoInfoRes.json();
+
+     const fetchLanguages = await fetch (`${repoInfo.languages_url}`);
+
+     const languageData = await fetchLanguages.json();
+
+     const languages = [];
+     for (let lang in languageData) {
+         languages.push(lang);
+     }
+
+     displayRepoInfo(repoInfo, languages);
+    };
+    
+    //Function to display specific info about a selected repo
+    const displayRepoInfo = function(repoInfo, languages) {
+        singleRepo.innerHTML = "";
+        const displayDiv = document.createElement("div");
+        displayDiv.innerHTML = `
+        <h3>Name: ${repoInfo.name}</h3>
+        <p>Description: ${repoInfo.description}</p>
+        <p>Default Branch: ${repoInfo.default_branch}</p>
+        <p>Languages: ${languages.join(", ")}</p>
+        <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+        `;
+        
+        singleRepo.append(displayDiv);
+
+        //Show a selected repo's data and hide the list of repo
+        singleRepo.classList.remove("hide");
+        repos.classList.add("hide");
+    };
